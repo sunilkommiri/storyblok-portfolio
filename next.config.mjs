b87b267bed6c5d885cd1114936bd130538fb9888
sunilkommuri <sunil1kommuri@gmail.com>
2025-06-17 10:29:47 -0500

    Add rewrite to map root path to homepage

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/homepage',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
